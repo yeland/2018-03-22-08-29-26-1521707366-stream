f25f3f8195d6a8c5fe6f83b6c36160fa55ce685d
yeland <wlx100715@163.com>
2019-03-01 12:11:27 +0800

    Complete some fuctions of add.

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,27 +2,62 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int evenSum = 0;
+        int left = (leftBorder > rightBorder) ? rightBorder : leftBorder;
+        int right = (leftBorder > rightBorder) ? leftBorder : rightBorder;
+        for (int i = left; i <= right; i++) {
+            if (i % 2 == 0) {
+                evenSum += i;
+            }
+        }
+        return evenSum;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int oddSum = 0;
+        int left = (leftBorder > rightBorder) ? rightBorder : leftBorder;
+        int right = (leftBorder > rightBorder) ? leftBorder : rightBorder;
+        for (int i = left; i <= right; i++) {
+            if (i % 2 == 1) {
+                oddSum += i;
+            }
+        }
+        return oddSum;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sum = 0;
+        for (Integer num : arrayList) {
+            sum += num * 3 + 2;
+        }
+        return sum;
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        for (Integer num : arrayList) {
+            if (num % 2 == 1) {
+                resultList.add(num * 3 + 2);
+            } else {
+                resultList.add(num);
+            }
+        }
+        return resultList;
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sum = 0;
+        for (Integer num : arrayList) {
+            if (num % 2 == 1) {
+                sum += num * 3 + 5;
+            }
+        }
+        return sum;
     }
 
     public double getMedianOfEvenIndex(List<Integer> arrayList) {
