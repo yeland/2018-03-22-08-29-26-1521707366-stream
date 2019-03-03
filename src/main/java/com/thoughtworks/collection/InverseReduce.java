b22e24f69cadb9842168e3a6003514a9d011701b
e <wlx100715@163.com>
2019-03-03 16:36:10 +0800

    Complete the function of InverseReduce.

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -9,13 +10,22 @@ public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        int step;
+        do {
+            step = this.random.nextInt(3);
+        } while (step == 0);
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = number - step; i >= 0; i = i - step) {
+            resultList.add(i);
+        }
+        return resultList;
     }
 }
