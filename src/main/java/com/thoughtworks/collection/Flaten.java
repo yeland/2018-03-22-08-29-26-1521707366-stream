03404dd4df3f48b81a7084e9eaf40d8a4fae74a0
e <wlx100715@163.com>
2019-03-03 15:38:04 +0800

    Complete the functures of Flaten.

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,20 +2,29 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            resultList.addAll(Arrays.asList(array[i]));
+        }
+        return resultList;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> resultList = transformToOneDimesional();
+        Filter filter = new Filter(resultList);
+        return filter.getDifferentElements();
     }
 }
