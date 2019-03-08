ba0280534e3e3ac3c3f26514cca8aebe796b1194
e <wlx100715@163.com>
2019-03-08 22:27:40 +0800

    Modify Add and Collection using Stream.

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -3,128 +3,68 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        int evenSum = 0;
-        int left = (leftBorder > rightBorder) ? rightBorder : leftBorder;
-        int right = (leftBorder > rightBorder) ? leftBorder : rightBorder;
-        for (int i = left; i <= right; i++) {
-            if (i % 2 == 0) {
-                evenSum += i;
-            }
-        }
-        return evenSum;
+        CollectionOperator collectionOperator = new CollectionOperator();
+        List<Integer> list = collectionOperator.getEvenListByIntervals(leftBorder, rightBorder);
+        return list.stream().reduce(0, (pre, cur) -> pre + cur);
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        int oddSum = 0;
-        int left = (leftBorder > rightBorder) ? rightBorder : leftBorder;
-        int right = (leftBorder > rightBorder) ? leftBorder : rightBorder;
-        for (int i = left; i <= right; i++) {
-            if (i % 2 == 1) {
-                oddSum += i;
-            }
-        }
-        return oddSum;
+        CollectionOperator collectionOperator = new CollectionOperator();
+        List<Integer> list = collectionOperator.getListByInterval(leftBorder, rightBorder);
+        return list.stream().filter(element -> element % 2 == 1).mapToInt(Integer::valueOf).sum();
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        int sum = 0;
-        for (Integer num : arrayList) {
-            sum += num * 3 + 2;
-        }
-        return sum;
+        return arrayList.stream().map(number -> number * 3 + 2).mapToInt(Integer::valueOf).sum();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        List<Integer> resultList = new ArrayList<>();
-        for (Integer num : arrayList) {
-            if (num % 2 == 1) {
-                resultList.add(num * 3 + 2);
-            } else {
-                resultList.add(num);
-            }
-        }
-        return resultList;
+        return arrayList.stream().filter(i -> i % 2 == 1).map(number -> number * 3 + 2).collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        int sum = 0;
-        for (Integer num : arrayList) {
-            if (num % 2 == 1) {
-                sum += num * 3 + 5;
-            }
-        }
-        return sum;
+        return arrayList.stream().filter(i -> i % 2 == 1).map(n -> n * 3 + 5).reduce(0, (a, b) -> a + b);
     }
 
     public double getMedianOfEvenIndex(List<Integer> arrayList) {
-        List<Integer> evenArrayList = new ArrayList<>();
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (i % 2 == 0) {
-                evenArrayList.add(arrayList.get(i));
-            }
-        }
-        Collections.sort(evenArrayList);
-        int small = (int) Math.floor((evenArrayList.size() - 1) / 2.0);
-        int large = (int) Math.ceil((evenArrayList.size() - 1) / 2.0);
-        return (evenArrayList.get(small) + evenArrayList.get(large)) / 2.0;
+        List<Integer> evenArrayList = IntStream.range(0, arrayList.size())
+                .filter(i -> i % 2 == 0).mapToObj(arrayList::get).collect(Collectors.toList());
+        Reduce reduce = new Reduce(evenArrayList);
+        return reduce.getOrderedMedian();
     }
 
     public double getAverageOfEvenIndex(List<Integer> arrayList) {
-        double sum = 0;
-        int length = 0;
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (i % 2 == 0) {
-                sum += arrayList.get(i);
-                length += 1;
-            }
-        }
-        return sum / length;
+        List<Integer> evenArrayList = IntStream.range(0, arrayList.size())
+                .filter(i -> i % 2 == 0).mapToObj(arrayList::get).collect(Collectors.toList());
+        return evenArrayList.stream().collect(Collectors.averagingDouble(Integer::valueOf));
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        for (Integer num : arrayList) {
-            if (num % 2 == 0 && specialElment.equals(num)) {
-                return true;
-            }
-        }
-        return false;
+        return arrayList.stream().filter(i -> i % 2 == 0).anyMatch(i -> i.equals(specialElment));
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        List<Integer> evenList = new ArrayList<>();
-        for (Integer num:arrayList) {
-            if(num%2==0 && evenList.indexOf(num)<0) {
-                evenList.add(num);
-            }
-        }
-        return evenList;
+        return arrayList.stream().filter(i -> i % 2 == 0).distinct().collect(Collectors.toList());
     }
 
     public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
-        List<Integer> evenList = new ArrayList<>();
-        List<Integer> oddList = new ArrayList<>();
-        for (Integer num:arrayList) {
-            if(num%2 == 0) {
-                evenList.add(num);
-            } else {
-                oddList.add(num);
-            }
-        }
-        Collections.sort(evenList);
-        Collections.sort(oddList);
-        Collections.reverse(oddList);
-        evenList.addAll(oddList);
-        return evenList;
+        List<Integer> evenList = arrayList.stream().filter(i -> i % 2 == 0)
+                .sorted().collect(Collectors.toList());
+        List<Integer> oddList = arrayList.stream().filter(i -> i % 2 == 1)
+                .sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        return Stream.concat(evenList.stream(),oddList.stream()).collect(Collectors.toList());
     }
 
     public List<Integer> getProcessedList(List<Integer> arrayList) {
-        List<Integer> array = new ArrayList<>();
-        for (int i = 1; i<arrayList.size();i++) {
-            array.add(3*(arrayList.get(i-1)+arrayList.get(i)));
-        }
+        IntStream indexStream = IntStream.range(1,arrayList.size());
+        List<Integer> array = indexStream.mapToObj(i->3*(arrayList.get(i-1)+arrayList.get(i)))
+                .collect(Collectors.toList());
         return array;
     }
 }
--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,70 +2,46 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        List<Integer> array = new ArrayList<>();
         if (left < right) {
-            for (int i = left; i <= right; i++) {
-                array.add(i);
-            }
-        } else {
-            for (int i = left; i >= right; i--) {
-                array.add(i);
-            }
+            return IntStream.rangeClosed(left, right).boxed().collect(Collectors.toList());
         }
-        return array;
+        List<Integer> list = IntStream.rangeClosed(right, left).sorted().boxed().collect(Collectors.toList());
+        list.sort(Comparator.reverseOrder());
+        return list;
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
         List<Integer> list = new ArrayList<>(getListByInterval(left, right));
-        List<Integer> evenList = new ArrayList<>();
-        for (Integer num:list) {
-            if(num%2==0) {
-                evenList.add(num);
-            }
-        }
-        return evenList;
+        return list.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        List<Integer> evenArray = new ArrayList<>();
-        for (int num : array) {
-            if (num % 2 == 0) {
-                evenArray.add(num);
-            }
-        }
-        return evenArray;
+        IntStream stream = Arrays.stream(array);
+        return stream.filter(i -> i % 2 == 0).boxed().collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
-        return array[array.length-1];
+        return array[array.length - 1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        List<Integer> common = new ArrayList<>();
-        List<Integer> secondList = new ArrayList<>();
-        for (int num:secondArray) {
-            secondList.add(num);
-        }
-        for (int num:firstArray) {
-            if(secondList.contains(num)) {
-                common.add(num);
-            }
-        }
-        return common;
+        IntStream firstStream = Arrays.stream(firstArray);
+        List<Integer> secondList = Arrays.stream(secondArray).boxed().collect(Collectors.toList());
+        return firstStream.filter(secondList::contains).boxed().collect(Collectors.toList());
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
         List<Integer> common = new ArrayList<>();
         List<Integer> firstList = Arrays.asList(firstArray);
         common.addAll(firstList);
-        for (Integer num:secondArray) {
-            if(!common.contains(num)) {
+        for (Integer num : secondArray) {
+            if (!common.contains(num)) {
                 common.add(num);
             }
         }
